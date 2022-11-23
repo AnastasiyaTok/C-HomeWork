2fc2a9dcc2b0e2a58e088e640a2ac3032ac42a0f
«Anastasiya» <anastasiya@exemple.com>
2022-11-23 22:21:26 +0300

    Task8 done

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Task8
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while(i <= n)
+{
+    Console.Write($"{i} ");
+    i+=2;
+}
