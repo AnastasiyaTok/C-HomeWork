6e3e518a9bd531157a6552c90659d95afdb9c8c1
«Anastasiya» <anastasiya@exemple.com>
2022-11-23 12:16:56 +0300

    Task6 done

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Task6
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+ if (a % 2 == 0)
+    Console.Write( $" Четное число ");
+else
+    Console.Write( $" Нечетное число ");
+
