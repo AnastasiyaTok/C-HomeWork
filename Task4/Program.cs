1dea75b0b4bcd6283b4ed01dbdcad628aabb8a83
«Anastasiya» <anastasiya@exemple.com>
2022-11-23 12:02:15 +0300

    Task4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Task4
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+ Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+    Console.Write( $" {a} Max число ");
+else if (b > c)
+    Console.Write( $" {b} Max число ");
+else
+    Console.Write( $" {c} Max число ");
