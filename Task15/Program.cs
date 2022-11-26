cce8595091dd04e9fc7ee3d2d5220f87d99c0e22
«Anastasiya» <anastasiya@exemple.com>
2022-11-26 23:43:31 +0300

    Task15 done

---
+++ Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿//Task15
+Console.Clear();
+Console.WriteLine("Введите число от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 6 || n == 7)
+{
+    Console.WriteLine("Выходной день! Ура!");
+}
+else
+    Console.Write("Будний день...");
