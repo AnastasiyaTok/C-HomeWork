2cb85568108453ebe9819236c868ce45c397b1fd
«Anastasiya» <anastasiya@exemple.com>
2022-11-26 13:35:25 +0300

    Task14 seminar done

---
+++ Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿//Task14
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 7 == 0);
+
+    if (n % 23 == 0)
+    Console.WriteLine("yes");
+else 
+    Console.Write("no");
+else
+    Console.Write("no");
