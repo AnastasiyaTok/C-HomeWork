558020b48f85e28e75754d32c283add7143a2de0
«Anastasiya» <anastasiya@exemple.com>
2022-11-26 13:26:33 +0300

    Task11 done seminar

---
+++ Task11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Task11
+Console.Clear();
+int n = new Random().Next(100, 1000);
+Console.Write($"Введите случайное число: {n} ");
+int n1 = n / 100;
+int n3 = n % 10;
+Console.WriteLine(n1 * 10 + n3);
+
