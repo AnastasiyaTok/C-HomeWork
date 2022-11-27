392395261fbf82def32c11e624e25fb5cca53f2d
«Anastasiya» <anastasiya@exemple.com>
2022-11-27 23:30:27 +0300

    Task13 done Alleluia

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿//Task13
+Console.Clear();
+Console.Write($"Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while ( n > 999) n /= 10;
+
+    Console.Write($"{(n % 10)}"); 
+
+    if (n < 99)
+        Console.WriteLine("Нет третьего числа");
+                
+
+   
