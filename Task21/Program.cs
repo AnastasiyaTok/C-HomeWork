ae6c5aac0ef2b08a63225fa6976cc98820771098
«Anastasiya» <anastasiya@exemple.com>
2022-11-29 18:29:59 +0300

    Task21 done

---
+++ Task21/Program.cs
@@ -0,0 +1,14 @@
+﻿//Task21
+Console.Clear();
+Console.WriteLine("Введите координату первой точки A: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+double ya = Convert.ToDouble(Console.ReadLine());
+double za = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки B: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+double yb = Convert.ToDouble(Console.ReadLine());
+double zb = Convert.ToDouble(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2) + Math.Pow(za - zb, 2));
+Console.WriteLine($"Расстояние между точками: {result}");
+
