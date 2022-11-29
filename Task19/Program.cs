4289e752064f1bbe0bf7f0cfdeb240a88edac8ed
«Anastasiya» <anastasiya@exemple.com>
2022-11-29 17:47:24 +0300

    Task19 done

---
+++ Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Task19
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string n = Console.ReadLine();
+int len = n.Length;
+
+if (len == 5)
+{
+    if (n [0] == n [4] && n [1] == n [3])
+    {
+        Console.WriteLine($"{n} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{n} - Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"Введенное число: {n} - не является пятизначным");
+}
