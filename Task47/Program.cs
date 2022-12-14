4baff32f19fbbb363ca617167ebc0307c2514a2c
«Anastasiya» <anastasiya@exemple.com>
2022-12-14 22:21:07 +0300

    Task47 done

---
+++ Task47/Program.cs
@@ -0,0 +1,24 @@
+﻿// Task47
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+Console.Clear();
+Console.WriteLine("Введите количество строк:  ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[row, column];
+InputMatrix(matrix);
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
