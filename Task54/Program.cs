1e0a29396a2ea2bdea7e72517cd86201a92ba6b6
«Anastasiya» <anastasiya@exemple.com>
2022-12-18 13:21:44 +0300

    Task54 done

---
+++ Task54/Program.cs
@@ -0,0 +1,53 @@
+﻿// Task54
+// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SortToLine(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Random random = new Random();
+int[,] matrix = new int[3, 4];
+Console.WriteLine("Исходный массив: ");
+InputMatrix(matrix);
+ReleaseMatrix(matrix);
+SortToLine(matrix);
+Console.WriteLine("Отсортированный массив: ");
+ReleaseMatrix(matrix);
